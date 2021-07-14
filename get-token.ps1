55783cd58e6fa772217895ac4a6ef7a148cb5d03
Max Melcher <melcher.maximilian@gmail.com>
2021-07-14 09:54:51 +0200

    fixing the script to get the token

--- get-token.ps1
+++ get-token.ps1
@@ -1,3 +1,3 @@
 az login --allow-no-subscriptions --use-device-code
-az account get-access-token --resource api://253600a6-46b5-425f-ab7e-d70df34c97ae
+$bearer = az account get-access-token --resource api://253600a6-46b5-425f-ab7e-d70df34c97ae --query 'accessToken' -o tsv
 git config http.extraHeader "MFA: bearer $($bearer)"
